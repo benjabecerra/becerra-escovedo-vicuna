1b796c912e77d081e22350d7f258939321094f89
favicuna <34049107+favicuna@users.noreply.github.com>
2019-08-31 20:38:52 -0400

    Update portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -1,5 +1,5 @@
  var trabajos = [{
-     figure: "Media/Benja1.jpg",
+     figure: "media/Benja1.jpg",
      alt: "Descripción de la imagen",
      title: "Título de pjAJAjajAajjAJAAJrimer trabajo",
      about: "Texto simulado de descripción de primer trabajo",
@@ -9,7 +9,7 @@
      
 
  }, {
-     figure: "Media/Vicu4.jpg",
+     figure: "media/Vicu4.jpg",
      alt: "Descripción de la imagen",
      title: "Título de segundo trabajo",
      about: "Texto simulado de descripción de segundo trabajo",
@@ -17,35 +17,35 @@
      color_fondo: "#2d23ea",
      
  }, {
-     figure: "Media/Paula1.jpg",
+     figure: "media/Paula1.jpg",
      alt: "Descripción de la imagen",
      title: "Título de tercer trabajo",
      about: "Texto simulado de descripción de tercer trabajo",
      color_letra: "Black",
      color_fondo: "#f49595",
  }, {
-     figure: "Media/Vicu2.jpg",
+     figure: "media/Vicu2.jpg",
      alt: "Descripción de la imagen",
      title: "Título de cuarto trabajo",
      about: "Texto simulado de descripción de cuarto trabajo",
      color_letra: "white",
      color_fondo: "#2d23ea",
  }, {
-     figure: "Media/Benja2.jpg",
+     figure: "media/Benja2.jpg",
      alt: "Descripción de la imagen",
      title: "Título de quinto trabajo",
      about: "Texto simulado de descripción de quinto trabajo",
      color_letra: "white",
      color_fondo: "#8100ea",
  }, {
-     figure: "Media/Vicu1.jpg",
+     figure: "media/Vicu1.jpg",
      alt: "Descripción de la imagen",
      title: "PEPS: Prepara, Emplata, Porciona",
      about: "Sistema lúdico de organización para la comida de los niños marca ILKO, con el fin de promover una alimentación porcionada y la participación de los mismos en la preparación.",
      color_letra: "white",
      color_fondo: "#2d23ea",
  }, {
-     figure: "Media/Paula2.jpg",
+     figure: "media/Paula2.jpg",
      alt: "Descripción de la imagen",
      title: "Título de séptimo trabajo",
      about: "Texto simulado de descripción de séptimo trabajo",
@@ -54,7 +54,7 @@
      
  },
  {
-    figure: "Media/Paula3.jpg",
+    figure: "media/Paula3.jpg",
     alt: "Descripción de la imagen",
     title: "Título de séptimo trabajo",
     about: "Texto simulado de descripción de séptimo trabajo",
@@ -62,14 +62,14 @@
      color_fondo: "#f49595",
 },
 {
-    figure: "Media/Benja3.jpg",
+    figure: "media/Benja3.jpg",
     alt: "Descripción de la imagen",
     title: "Título de séptimo trabajo",
     about: "Texto simulado de descripción de séptimo trabajo",
     color_letra: "white",
      color_fondo: "#8100ea",
 },{
-    figure: "Media/Benja4.jpg",
+    figure: "media/Benja4.jpg",
     alt: "Descripción de la imagen",
     title: "Título de séptimo trabajo",
     about: "Texto simulado de descripción de séptimo trabajo",
@@ -77,7 +77,7 @@
      color_fondo: "#8100ea",
 }
 ,{
-    figure: "Media/Paula4.jpg",
+    figure: "media/Paula4.jpg",
     alt: "Descripción de la imagen",
     title: "Título de séptimo trabajo",
     about: "Texto simulado de descripción de séptimo trabajo",
